ab00fc17d1c40cbd6a52eb15e6964ff63886d355
Slowlylee1031 <slowly.s.lee@gmail.com>
2024-11-29 16:12:59 +0800

    Add files via upload

--- product.js
+++ product.js
@@ -23,7 +23,7 @@ const productSchema = new mongoose.Schema({
     name: {type:String, require: true},
     price: {type: Number, require: true},
     discount: {type: Number, default:0},
-    newItem: {type: Boolean},
+    newItem: {type: Boolean, require:true},
     colorIcon:[{type:String}],
     sku: [{
             color: {type:String},
@@ -41,28 +41,47 @@ const productSchema = new mongoose.Schema({
 
 const product = mongoose.model('product', productSchema);
 
-//Get all products
-
-app.get('/products', async (req, res) => {
+//Get one product
+app.get('/products/:name', async (req, res) => {
+    
     try {
-        const productItems = await getProducts();
-        res.json(productItems);
+        const product = await getOneProduct(req.params.name);
+        res.json(product);
     } catch (error) {
         console.error('Server error:', error);
         res.status(500).json({ message: 'Server connection error. Please try again later.', error: error.message });
     }
 });
 
-const getProducts = async (req,res) => {
+//Get all products
+
+app.get('/products', async (req, res) => {
+    try {
+        const productItems = await getProducts();
+        res.json(productItems);
+           console.log(product);
+    } catch (error) {
+        console.error('Server error:', error);
+        res.status(500).json({ message: 'Server connection error. Please try again later.', error: error.message });
+    }
+});
+
+const getProducts = async () => {
     const products = await product.find({
         'sku.Unit.qty': { $gt: 0 }
     });
-    console.log(products.length);
+ 
     return products;
 };
 
 
-
+const getOneProduct = async (name) => {
+    const productItem = await product.find(
+        {name}
+    );
+    console.log(productItem);
+    return productItem;
+};
 
 app.listen(port, () => {
     console.log(`Server running at http://${host}:${port}`);
