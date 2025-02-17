3ee4f5f338d18f85139444a48c64a8d1c2fe6a35
Slowlylee1031 <slowly.s.lee@gmail.com>
2025-02-17 11:44:09 +0800

    Add files via upload

--- product.js
+++ product.js
@@ -1,48 +1,10 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const app = express();
-const cors = require('cors');
-const port = 3001;
-const host = 'localhost';
+const product = require('../models/product')
 
-app.use(express.json());
-app.use(express.static(__dirname+'/public'))
-app.use(cors());
+const router = new express.Router()
 
-mongoose.connect('mongodb://localhost:27017/projectdb')
-.then( () => {
-    console.log("Database successfully connected!");
-} )
-.catch( (error) => {
-    console.error("Database connected fails, please try again later.")
-});
-
-//set Schema
-
-const productSchema = new mongoose.Schema({
-    name: {type:String, require: true},
-    price: {type: Number, require: true},
-    discount: {type: Number, default:0},
-    newItem: {type: Boolean, require:true},
-    colorIcon:[{type:String}],
-    sku: [{
-            color: {type:String},
-            image: [{type:String}],
-            Unit: [{size: {type: Number}, qty:{type: Number}},]
-    }],
-    details:{type:String},
-    length:{type:String},
-    fit:{type:String},
-    style : {type:String},
-    sleeveStyle :{type:String},
-    color :{type:String}
-
-});
-
-const product = mongoose.model('product', productSchema);
-
-//Get one product
-app.get('/products/:name', async (req, res) => {
+//getOneProduct(req,res)
+router.get('/:name', async (req, res) => {
     
     try {
         const product = await getOneProduct(req.params.name);
@@ -53,18 +15,19 @@ app.get('/products/:name', async (req, res) => {
     }
 });
 
-//Get all products
-
-app.get('/products', async (req, res) => {
+//getAllProducts(req,res)
+router.get('/', async (req, res) => {
     try {
         const productItems = await getProducts();
         res.json(productItems);
-           console.log(product);
+        //console.log(product);
     } catch (error) {
-        console.error('Server error:', error);
+        console.error('routerServer error:', error);
         res.status(500).json({ message: 'Server connection error. Please try again later.', error: error.message });
     }
 });
+
+//-------------------------------------------------------------------
 
 const getProducts = async () => {
     const products = await product.find({
@@ -79,10 +42,28 @@ const getOneProduct = async (name) => {
     const productItem = await product.find(
         {name}
     );
-    console.log(productItem);
+    // console.log(productItem);
     return productItem;
 };
 
-app.listen(port, () => {
-    console.log(`Server running at http://${host}:${port}`);
+const getCategoryItems = (categories) => {
+    console.log('categories',categories)
+    return items.filter(item => item.category === categories);
+};
+
+// Route to get items by category
+router.get('/:categories', async (req, res) => {
+    console.log('req',req)
+    try {
+        const products = await getCategoryItems(req.params.category);
+        console.log('products', products);
+        res.json(products);
+    } catch (error) {
+        console.error('Server error:', error);
+        res.status(500).json({ message: 'Server connection error. Please try again later.', error: error.message });
+    }
 });
+
+
+
+module.exports = router
